93762cb8466a0e9806fe6c6a0eed605ef25a007f
Didac Esteban Andres <didacestebanandres@Didacs-MacBook-Pro.local>
2017-07-02 17:34:04 +0200

    new folders structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './views/style/css/index.css';
-import App from './views/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
